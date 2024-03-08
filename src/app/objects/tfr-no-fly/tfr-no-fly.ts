35f3edf1fc7ab5d53ca18fcea4d9df2498f1e435
Eduardo Montoya <36909185+edmontoya1@users.noreply.github.com>
2024-03-07 18:44:09 -0600

    Feature/tfr height (#12)
    * added altitude to TfrNoFly class
    
    * altitude now stored in array

--- src/app/objects/tfr-no-fly/tfr-no-fly.ts
+++ src/app/objects/tfr-no-fly/tfr-no-fly.ts
@@ -1,9 +1,9 @@
 export class TfrNoFly {
-
-    notamNumber: string;
-    notamType: string;
-    latlong: Number[];
-    radius: number;
-    startTime: string;
-    endTime: string;
+  notamNumber: string;
+  notamType: string;
+  latlong: Number[];
+  radius: number;
+  startTime: string;
+  endTime: string;
+  altitude: Number[];
 }
