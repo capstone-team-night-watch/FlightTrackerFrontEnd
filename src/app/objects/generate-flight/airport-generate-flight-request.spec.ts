e548e47aa1e450bc67e12424520abd2616161c6d
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-03-11 10:56:46 -0500

    created tests for airport-generate-flight-request

--- src/app/objects/generate-flight/airport-generate-flight-request.spec.ts
+++ src/app/objects/generate-flight/airport-generate-flight-request.spec.ts
@@ -1,7 +1,35 @@
 import { AirportGenerateFlightRequest } from './airport-generate-flight-request';
 
-describe('GetNoFlyZonesResponse', () => {
+describe('AirportGenerateFlightRequest', () => {
+  let request: AirportGenerateFlightRequest;
+
+  beforeEach(() => {
+    request = new AirportGenerateFlightRequest();
+  });
+
   it('should create an instance', () => {
     expect(new AirportGenerateFlightRequest()).toBeTruthy();
+  });
+
+  it('should return airline name if set', () => {
+    let testName = 'Airline Test Name';
+    request.setAirlineName(testName);
+    expect(request.getAirlineName()).toEqual(testName);
+  });
+
+  it('should return falsy if airline name not set', () => {
+    let airlineName = request.getAirlineName();
+    expect(airlineName).toBeFalsy();
+  });
+
+  it('should return flight icao if set', () => {
+    let flightIcao = 'Flight Acao';
+    request.setFlightIcao(flightIcao);
+    expect(request.getFlightIcao()).toEqual(flightIcao);
+  });
+
+  it('should return falsy if icao not set', () => {
+    let icao = request.getFlightIcao();
+    expect(icao).toBeFalsy();
   });
 });
