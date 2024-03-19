9297b44f55e9df1b7a24dd7e0c064d2d138aed3a
jrettingeruno <124631514+jrettingeruno@users.noreply.github.com>
2024-03-18 19:54:26 -0500

    Changed to use sim controller instead of cesium service

--- src/app/flight-tracker-dashboard/flight-tracker-dashboard.component.ts
+++ src/app/flight-tracker-dashboard/flight-tracker-dashboard.component.ts
@@ -5,6 +5,7 @@ import { Url } from '../../lib/utils/url';
 import { CesiumService } from '../cesium.service';
 import { HttpClient } from '@angular/common/http';
 import { CesiumComponentComponent } from '../cesium-component/cesium-component.component';
+import { CesiumSimulationController } from '../simulation.controller';
 
 @Component({
   selector: 'app-flight-tracker-dashboard',
@@ -15,20 +16,43 @@ export class FlightTrackerDashboardComponent implements OnInit {
   private simulationController: SimulationController;
   @ViewChild('simulationRenderer') simulationRenderer: CesiumComponentComponent ;
 
-  constructor(private _snackBar: MatSnackBar, private cesium: CesiumService) {}
+  constructor(private _snackBar: MatSnackBar, private cesiumSimulationController: CesiumSimulationController, private cesium: CesiumService) {}
 
   ngOnInit() {
+    
     try {
       this.simulationController = new SimulationController();
     } catch (exception) {
       this._snackBar.open('Failed to connect to consumer socket', 'Close');
     }
+    
 
     this.simulationController.onColision(() => {
       this._snackBar.open('Collision Detected', 'Close');
     });
 
-    this.cesium.setUpViewer('cesium');
-    this.cesium.getAndLoadNoFlyZones();
+    //PLACEHOLDER
+    /*
+    this.cesiumSimulationController.placeholderRepository.setUpRepository();
+    let testFlight = this.cesiumSimulationController.placeholderRepository.createTestFlight();
+    
+    this.cesiumSimulationController.updateFlightsAndZones(
+      "cesium",
+      testFlight.last_position.latitude,
+      testFlight.last_position.longitude,
+      testFlight.last_position.altitude,
+      testFlight.ident_icao,
+      undefined,
+      testFlight
+    );
+    */
+    
+    this.cesiumSimulationController.cesiumViewController.setUpViewer("cesium");
+    this.cesiumSimulationController.getAndLoadNoFlyZones();
+    this.cesiumSimulationController.getAndLoadTfrNoFlyZones();
+    
+    //PREVIOUS SETUP
+    //this.cesium.setUpViewer('cesium');
+    //this.cesium.getAndLoadNoFlyZones();
   }
 }
