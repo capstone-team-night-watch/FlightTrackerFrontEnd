9f7ecd33c45e033acd4df046857389ee8bd25a4b
ewags2 <114793424+ewags2@users.noreply.github.com>
2024-03-22 12:31:06 -0500

    good urls for build

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    socketUrl : 'http://localhost:4000',
-    consumerBaseUrl: 'http://localhost:5000',
+    consumerBaseUrl: 'http://ec2-13-58-87-154.us-east-2.compute.amazonaws.com:5000',
+    producerBaseUrl : 'http://ec2-13-58-87-154.us-east-2.compute.amazonaws.com:6000',
+    socketUrl : 'http://ec2-13-58-87-154.us-east-2.compute.amazonaws.com:4000',
 };
