f04fec7940c1dac0783311bf472c759b14084023
ewags2 <114793424+ewags2@users.noreply.github.com>
2024-03-22 01:04:43 -0500

    revert back

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    consumerBaseUrl: './api/consumer',
-    producerBaseUrl : './localhost/api/producer',
-    socketUrl : '/localhost/api/websocket',
+    consumerBaseUrl: 'http://ec2-13-58-87-154.us-east-2.compute.amazonaws.com:5000',
+    producerBaseUrl : 'http://ec2-13-58-87-154.us-east-2.compute.amazonaws.com:6000',
+    socketUrl : 'http://ec2-13-58-87-154.us-east-2.compute.amazonaws.com:4000',
 };
