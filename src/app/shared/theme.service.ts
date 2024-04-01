efdf35dde13d9bda50e41accf5f9fbfa22c6cc8f
jrettingeruno <124631514+jrettingeruno@users.noreply.github.com>
2024-04-01 07:41:20 -0500

    New colors for paths and airports

--- src/app/shared/theme.service.ts
+++ src/app/shared/theme.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ThemeService {
   public pathColor = 'white';
+  public alternatePathColor = 'yellow';
+  public airportColor = 'green';
   public noFlyZoneColor = '#f02b358c';
 
   constructor() {}
