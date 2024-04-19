83753f9333efb8918b89c756840d34ead66fc4ff
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-04-18 23:42:44 -0500

    refactored plan position tests

--- src/lib/simulation-entities/PlanePosition.spec.ts
+++ src/lib/simulation-entities/PlanePosition.spec.ts
@@ -2,16 +2,20 @@ import { Cartesian3, JulianDate } from 'cesium';
 import { DynamicPlanePosition } from './PlanePosition';
 
 describe('DynamicPlanePosition', () => {
+  let dynamicPosition: DynamicPlanePosition;
+
+  const coordinates = {
+    longitude: -75.0,
+    latitude: 40.0,
+    altitude: 1000.0,
+  };
+
+  beforeEach(() => {
+    dynamicPosition = new DynamicPlanePosition(coordinates);
+  });
+
   // Test case for initializing the DynamicPlanePosition
   it('should initialize with correct coordinates', () => {
-    const coordinates = {
-      longitude: -75.0,
-      latitude: 40.0,
-      altitude: 1000.0,
-    };
-
-    const dynamicPosition = new DynamicPlanePosition(coordinates);
-
     // Verify that the initial coordinates are converted to Cartesian3 correctly
     const expectedCartesian = Cartesian3.fromDegrees(coordinates.longitude, coordinates.latitude, coordinates.altitude);
     expect(dynamicPosition.getCoordinates()).toEqual(expectedCartesian);
@@ -19,19 +23,11 @@ describe('DynamicPlanePosition', () => {
 
   // Test case for updating coordinates
   it('should update coordinates correctly', () => {
-    const initialCoordinates = {
-      longitude: -75.0,
-      latitude: 40.0,
-      altitude: 1000.0,
-    };
-
     const updatedCoordinates = {
       longitude: -80.0,
       latitude: 35.0,
       altitude: 1500.0,
     };
-
-    const dynamicPosition = new DynamicPlanePosition(initialCoordinates);
 
     // Update coordinates
     dynamicPosition.setCoordinates(updatedCoordinates);
@@ -47,13 +43,6 @@ describe('DynamicPlanePosition', () => {
 
   // Test case for getValue method
   it('should return correct Cartesian3 value for a given time', () => {
-    const coordinates = {
-      longitude: -75.0,
-      latitude: 40.0,
-      altitude: 1000.0,
-    };
-    const dynamicPosition = new DynamicPlanePosition(coordinates);
-
     const currentTime = JulianDate.now();
 
     // Call getValue method with current time
